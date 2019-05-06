5da079ecdcd2fdcbf56e25e426d52763fb7a3ed8
David Cruz <dfcc15@hotmail.com>
2019-05-05 21:51:00 -0500

    Add files via upload

---
+++ for_basico.java
@@ -0,0 +1,16 @@
+package switch_case;
+
+public class for_basico {
+
+   public static void main(String[] args)
+	{
+		//imprime los numeros 
+		System.out.println("Numeros del 1 al 10: ");
+
+		//ciclo FOR loop  to print 1 to 10.
+		for(int i = 1; i <= 10; i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
